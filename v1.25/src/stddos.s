c3fa0fbfe38241c93e6537316ef9e31f33a8d685
Eli Weatherby <chemicalorca22@gmail.com>
2025-08-18 00:34:34 -0500

    On branch master  Your branch is ahead of 'MS-DOS/master' by 3 commits.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   source/ASM.ASM
    	modified:   source/dos1.25.sublime-workspace
    	new file:   src/command.s
    	new file:   src/stddos.s

---
+++ v1.25/src/stddos.s
@@ -0,0 +1,21 @@
+        TITLE   MS-DOS version 1.25 by Tim Paterson     March 3, 1982
+        PAGE    60,132
+# Use the following booleans to set the switches 
+.equ    FALSE,  0
+.equ    TRUE,   NOT FALSE
+
+# Use the switches below to produce the standard Microsoft version of the IBM
+# version of the operating system
+.equ    MSVER,  TRUE
+.equ    IBM,    FALSE
+
+# Set this switch to cause DOS to move itself to the end of memory
+.equ    HIGHMEM, TRUE
+
+# Turn on switch below to allow testing disk code with DEBUG. It sets
+# up a different stack for disk I/O (functions > 11) than that used for
+# character I/O which effectively makes the DOS re-entrant.
+
+.equ    DSKTEST, FALSE
+
+        #include "MSDOS.ASM"
